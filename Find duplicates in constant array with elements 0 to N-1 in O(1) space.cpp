b37bfac72f0da29031ea985784da8ba13625fec3
Atharva Deshmukh <81254891+Atharva-Deshmukh@users.noreply.github.com>
2021-08-06 17:13:51 +0530

    Update Find duplicates in constant array with elements 0 to N-1 in O(1) space.cpp

--- Find duplicates in constant array with elements 0 to N-1 in O(1) space.cpp
+++ Find duplicates in constant array with elements 0 to N-1 in O(1) space.cpp
@@ -7,59 +7,35 @@ You must solve the problem without modifying the array nums (Indexing method use
 
 Approach:
 
-Use equations wali method later..
-
-XOR for misssing as well as missing
-
-EX: { 1, 3, 4, 5, 5, 6, 2 }  Rep => 5  Missing => 7
-
-//first XOR
-{ 1, 3, 4 ,6, 2 }
-
-//second XOR (with {1,2,3,4,5,6,7})
-{5,7}
+use sum formula
     
 
 */
 
 
-#include <bits/stdc++.h>
-using namespace std;
-
-void getTwoElements(int arr[], int size,int* x, int* y)
-{
-	int XOR1=0,XOR2=0,ans=0;
-	
-	for (int i = 0; i < size; i++) 
-	{
-		//duplicates jitne bhi hoge vo nullufy hue hoge ab sirf non repeating bache hai
-        XOR1=XOR1^arr[i];
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) 
+    {
+        int sum=0,ans;
+        int n=sizeof(nums)/sizeof(nums[0]);
+        
+        //getting sum of array elements
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+nums[i];
+        }
+        
+        //getting the sum of natural numbers from [1..n]
+        int orig_sum=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            orig_sum=orig_sum+(i+1);
+        }
+        
+        ans=orig_sum-sum;
+        return ans;
     }
-    
-    //ab apne ko pata hai ki each integer is in the range [1, n] inclusive. Inhi ke saath XOR lo and I will get answer
-    for (int i = 0; i < size; i++) 
-	{
-		XOR2=XOR2^(i+1);    //i 0 se start hoga apne ko [1, n] ke saath nikaalna hai
-    }
-    
-//our answer
-ans=XOR1^XOR2;
-cout<<ans;
-}
-
-/* Driver code */
-int main()
-{
-    int arr[] = { 1, 3, 4, 5, 5, 6, 2 };
-    
-    //apne ouputs
-    int* x = (int*)malloc(sizeof(int));
-    int* y = (int*)malloc(sizeof(int));
-    
-    int n = sizeof(arr) / sizeof(arr[0]);
- 
-    getTwoElements(arr, n, x, y);
-    cout << " The missing element is " << *x << " and the repeating"
-         << " number is " << *y;
-   
-}
+};
+};
